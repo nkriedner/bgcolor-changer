165ea2dbe04328cba36a4a2168174bd10f3ec1a0
Nils Kriedner <nkriedner@googlemail.com>
2020-03-22 00:34:15 +0100

    Added Color Changer DELUXE

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const input = document.querySelector("input");
+const change1 = document.querySelector("#change1");
 const change2 = document.querySelector("#change2");
+const change3 = document.querySelector("#change3");
 const body = document.querySelector("body");
 const select = document.querySelector("select");
+const colorChoiceDeluxe = document.querySelector("#colorChoiceDeluxe");
 
 change1.addEventListener("click", function() {
   body.style.background = select.value;
@@ -16,6 +19,10 @@ input.addEventListener("keyup", function() {
     // Cancel the default action, if needed
     event.preventDefault();
     // Trigger the button element with a click
-    button.click();
+    change2.click();
   }
+});
+
+change3.addEventListener("click", function() {
+  body.style.background = colorChoiceDeluxe.value;
 });
